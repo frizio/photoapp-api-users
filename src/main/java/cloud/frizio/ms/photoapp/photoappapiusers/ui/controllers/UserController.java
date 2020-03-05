518fbf02b7777a268d79840dfa0b164560b67778
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-05 17:06:34 +0100

    first commit

---
+++ src/main/java/cloud/frizio/ms/photoapp/photoappapiusers/ui/controllers/UserController.java
@@ -0,0 +1,21 @@
+package cloud.frizio.ms.photoapp.photoappapiusers.ui.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @Autowired
+  Environment env;
+
+  @GetMapping("/status/check")
+  public String status() {
+    return "Api users working on port " + env.getProperty("local.server.port");
+  }
+  
+}
---
+++ src/test/java/cloud/frizio/ms/photoapp/photoappapiusers/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.frizio.ms.photoapp.photoappapiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
