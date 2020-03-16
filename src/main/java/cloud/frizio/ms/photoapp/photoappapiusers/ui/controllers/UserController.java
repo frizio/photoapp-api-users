53462598167eb4503d059d65988e3dbf0cb7532b
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-16 18:22:16 +0100

    Create method

--- src/main/java/cloud/frizio/ms/photoapp/photoappapiusers/ui/controllers/UserController.java
+++ src/main/java/cloud/frizio/ms/photoapp/photoappapiusers/ui/controllers/UserController.java
@@ -3,8 +3,12 @@ package cloud.frizio.ms.photoapp.photoappapiusers.ui.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import cloud.frizio.ms.photoapp.photoappapiusers.ui.model.User;
 
 @RestController
 @RequestMapping("/users")
@@ -17,5 +21,10 @@ public class UserController {
   public String status() {
     return "Api users working on port " + env.getProperty("local.server.port");
   }
-  
+
+  @PostMapping
+  public String createUser(@RequestBody User user) {
+    return "User created!"; 
+  }
+
 }
